20766a02925df13b2823fb3b2933e7089404474f
alexisrc1 <alexis.rcarrassat@gmail.com>
2023-09-27 19:08:33 +0200

    change

---
+++ lessons/models/staging/stg_ecommerce__products.sql
@@ -0,0 +1,24 @@
+WITH source AS (
+	SELECT *
+
+	FROM {{ source('thelook_ecommerce', 'products') }}
+)
+
+SELECT
+	-- IDs
+	id AS product_id,
+
+	-- Other columns
+	cost,
+	retail_price,
+	department
+
+	{#- Unused columns:
+		- inventory_item_id
+		- distribution_center_id
+		- category
+		- sku
+		- name
+	#}
+
+FROM source
